8be0de2df1ac9114633b44bb166c53603837f5fb
Anjali Kumari <anjalikumari.dev@gmail.com>
2026-07-12 12:23:54 +0530

    Implemented user login authentication

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -40,6 +40,22 @@ def register(request):
 
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("/dashboard/")
+        else:
+            messages.error(request, "Invalid username or password")
+
     return render(request, "accounts/login.html")
 
 
