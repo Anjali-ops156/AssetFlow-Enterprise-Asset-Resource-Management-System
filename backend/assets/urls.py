3b12162f2a26a66113fb3d2973998f0fbe9c1664
Anjali Kumari <anjalikumari.dev@gmail.com>
2026-07-12 10:53:35 +0530

    Added asset listing page with search and filter

--- backend/assets/urls.py
+++ backend/assets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import asset_list
+from .views import asset_list, asset_detail
 
 urlpatterns = [
     path("", asset_list, name="asset_list"),
+    path("<int:pk>/", asset_detail, name="asset_detail"),
 ]
--- backend/assets/views.py
+++ backend/assets/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from .models import Asset
+from django.shortcuts import get_object_or_404
 
 
 def asset_list(request):
     assets = Asset.objects.all().order_by("-id")
+
+    search = request.GET.get("search")
+    status = request.GET.get("status")
+
+    if search:
+        assets = assets.filter(name__icontains=search)
+
+    if status:
+        assets = assets.filter(status=status)
 
     context = {
         "assets": assets
     }
 
     return render(request, "assets/asset_list.html", context)
+
+def asset_detail(request, pk):
+    asset = get_object_or_404(Asset, pk=pk)
+    return render(request, "assets/asset_detail.html", {"asset": asset})
